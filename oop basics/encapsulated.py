a43ef10103fb1b421945d211d98ac870e354ee02
Makz82 <mackz@tut.by>
2022-12-21 15:40:08 +0300

    Practice #5. The code

--- oop basics/encapsulated.py
+++ oop basics/encapsulated.py
@@ -10,22 +10,44 @@
 
 
 class A:
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.__value = value
 
     def __setattr__(self, key, value):
-        pass
+        if key == "_A__value":
+            self.__dict__[key] = value
+        else:
+            raise AttributeError
 
-    def set_value(self):
-        pass
+    def set_value(self, value):
+        self.__value = value
 
-    def get_value(self):
-        pass
+    @property
+    def value(self):
+        return self.__value
 
 
 def main():
-    pass
+    a = A(0)
+
+    print(a.value)
+
+    a.set_value(10)
+    print(a.value)
+
+    a.set_value(a.value * 5)
+    print(a.value)
+
+    print(a.__dict__)
+
+    print(a._A__value)
+
+    a._A__value = 22
+    print(a.value)
+
+    a.__value = 333
+    print(a.value)
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     main()
