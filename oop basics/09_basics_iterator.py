fafc9854f49cb59fd7dfa687c38cd589f31f6887
Makz82 <mackz@tut.by>
2022-12-25 19:40:16 +0300

    Practice #9. The task

---
+++ oop basics/09_basics_iterator.py
@@ -0,0 +1,25 @@
+# author: MLebedev
+""" Практика по ООП #9
+
+https://younglinux.info/oopython/iterator
+
+Напишите класс-итератор, объекты которого генерируют случайные числа в количестве и в диапазоне,
+которые передаются в конструктор.
+
+"""
+
+
+class Randi:
+    pass
+
+
+class NumIterator:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
