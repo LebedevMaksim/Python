6790334a2b47b54ec0bdcf871fbb5f4538c7ae3f
Makz82 <mackz@tut.by>
2022-12-26 16:40:19 +0300

    Practice #13. Basic code for classes

--- oop basics/13_basics_final.py
+++ oop basics/13_basics_final.py
@@ -10,17 +10,31 @@ https://younglinux.info/oopython/ooprogramm
 
 class Data:
     """Knowledge base. Elements can be retrieved by index."""
-    pass
+    def __init__(self, *info):
+        self.info = list(info)
+
+    def __getitem__(self, i):
+        return self.info[i]
 
 
 class Teacher:
     """The teacher must be able to transfer data from the knowledge base to the student or group"""
-    pass
+    def __init__(self):
+        self.work = 0
+
+    def teach(self, info, *pupil):
+        for i in pupil:
+            i.take(info)
+            self.work += 1
 
 
 class Pupil:
     """The student receives information and retains this knowledge. Maybe forget some data."""
-    pass
+    def __init__(self):
+        self.knowledge = []
+
+    def take(self, info):
+        self.knowledge.append(info)
 
 
 def main():
