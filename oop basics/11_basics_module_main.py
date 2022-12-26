fa86c02fbf7bda1a909a6b12c6b3ebf5799ec66d
Makz82 <mackz@tut.by>
2022-12-26 13:26:27 +0300

    Practice #11. The task

---
+++ oop basics/11_basics_module_main.py
@@ -0,0 +1,60 @@
+# author: MLebedev
+""" Практика по ООП #11
+
+https://younglinux.info/oopython/module
+
+В практической работе урока 7 "Композиция" требовалось разработать интерфейс
+взаимодействия с пользователем. Разнесите сам класс и интерфейс по разным файлам.
+Какой из них выполняет роль модуля, а какой – скрипта?
+Оба файла можно поместить в один каталог
+
+"""
+
+
+import math
+
+
+class WinDoor:
+    def __init__(self, x, y):
+        self.square = x * y
+
+
+class Room:
+    def __init__(self, x, y, z):
+        self.width, self.length, self.height = x, y, z
+        self.wd = []
+
+    def add_wd(self, w, h):
+        self.wd.append(WinDoor(w, h))
+
+    @property
+    def square(self):
+        return 2 * (self.width + self.length) * self.height
+
+    def work_surface(self):
+        square = self.square
+        for i in self.wd:
+            square -= i.square
+        return square
+
+
+def main():
+    x, y, h = map(float, input('Please enter the width, length and height, \nseparated by space:').split(sep=' '))
+    r1 = Room(x, y, h)
+    print('Room square:', r1.square)
+
+    wd_i = int(input('How many windows and doors there are in the room:'))
+    for i in range(wd_i):
+        a, b = map(float, input('Please enter the width and height, \nseparated by space:').split(sep=' '))
+        r1.add_wd(a, b)
+
+    work_s = r1.work_surface()
+    print('Work surface:', work_s)
+
+    r_w, r_h = map(float, input('Please enter the wallpaper width and height, \nseparated by space:').split(sep=' '))
+    r_count = math.ceil(work_s / (r_w * r_h))
+    print('Wallpaper rolls count:', r_count)
+
+
+if __name__ == '__main__':
+    main()
