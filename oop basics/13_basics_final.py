d6e63be727290c63280665cf072ff76b70be94f4
Makz82 <mackz@tut.by>
2022-12-26 15:55:02 +0300

    Practice #13. Final task.

---
+++ oop basics/13_basics_final.py
@@ -0,0 +1,31 @@
+# author: MLebedev
+""" Практика по ООП. Final
+
+https://younglinux.info/oopython/ooprogramm
+
+
+
+"""
+
+
+class Data:
+    """Knowledge base. Elements can be retrieved by index."""
+    pass
+
+
+class Teacher:
+    """The teacher must be able to transfer data from the knowledge base to the student or group"""
+    pass
+
+
+class Pupil:
+    """The student receives information and retains this knowledge. Maybe forget some data."""
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
