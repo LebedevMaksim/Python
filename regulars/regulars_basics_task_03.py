e04f689c6ee57cc2be7e7fc4aec5e04101f24768
Makz82 <mackz@tut.by>
2023-01-03 14:12:09 +0300

    Regular expressions - Task 3

---
+++ regulars/regulars_basics_task_03.py
@@ -0,0 +1,24 @@
+# Task 3
+# https://habr.com/ru/post/349860/#Zadachi__1
+
+
+import re
+
+
+text_a = """Иван Иванович! 
+Нужен ответ на письмо от ivanoff@ivan-chai.ru. 
+Не забудьте поставить в копию 
+serge'o-lupin@mail.ru- это важно.
+"""
+
+text_b = """foo.@ya.ru, foo@.ya.ru"""
+
+text_c = """boo@ya_ru, -boo@ya.ru-, foo№boo@ya.ru"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
