048f9749ac9455f71d7a4d87186a7350b869eafd
Makz82 <mackz@tut.by>
2023-01-04 19:22:40 +0300

    Regular expressions - Task 7 - Code

--- regulars/regulars_basics_task_07.py
+++ regulars/regulars_basics_task_07.py
@@ -1,4 +1,4 @@
-# Task 6
+# Task 7
 # https://habr.com/ru/post/349860/#Zadachi__1
 
 
@@ -9,9 +9,14 @@ text = """Было закуплено 12 единиц техники
 по 410.37 рублей."""
 
 
-def main():
-    pass
+def cube_match(match):
+    return str(int(match[0]) ** 3)
+
+
+def main(text_):
+    coded = re.sub(r'\d+', cube_match, text_, flags=re.MULTILINE)
+    print(coded)
 
 
 if __name__ == '__main__':
-    main()
+    main(text)
