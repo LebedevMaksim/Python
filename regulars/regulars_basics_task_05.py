fa3a7aa6feeb05b93772818611d6afac9c1d45af
Makz82 <mackz@tut.by>
2023-01-04 15:44:42 +0300

    Regular expressions - Task 5

---
+++ regulars/regulars_basics_task_05.py
@@ -0,0 +1,24 @@
+# Task 4
+# https://habr.com/ru/post/349860/#Zadachi__1
+
+
+import re
+
+
+text = """1.2 
+  1. 
+    1.0e-55  
+      e-12   
+  6.5E 
+        1e-12  
+  +4.1234567890E-99999           
+  7.6e+12.5 
+   99 """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
