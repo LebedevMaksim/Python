586e46f21fdd39a107399b13027231bd198206a0
Makz82 <mackz@tut.by>
2023-01-04 15:41:00 +0300

    Regular expressions - Task 4 - code

--- regulars/regulars_basics_task_04.py
+++ regulars/regulars_basics_task_04.py
@@ -2,14 +2,17 @@
 # https://habr.com/ru/post/349860/#Zadachi__1
 
 
+import re
+
+
 text = """Уважаемые! Если вы к 09:00 не вернёте 
 чемодан, то уже в 09:00:01 я за себя не отвечаю. 
 PS. С отношением 25:50 всё нормально!"""
 
 
-def main():
-    pass
+def main(text_):
+    print(re.sub(r'(?:(?:[0-1][0-9])|(?:2[0-4]))(?::[0-5][0-9]){,2}', 'TBD', text_))
 
 
 if __name__ == '__main__':
-    main()
+    main(text)
