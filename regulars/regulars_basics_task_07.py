adb95804245caa3712182e9de299eab4a6367d01
Makz82 <mackz@tut.by>
2023-01-04 19:08:58 +0300

    Regular expressions - Task 7

---
+++ regulars/regulars_basics_task_07.py
@@ -0,0 +1,17 @@
+# Task 6
+# https://habr.com/ru/post/349860/#Zadachi__1
+
+
+import re
+
+
+text = """Было закуплено 12 единиц техники 
+по 410.37 рублей."""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
