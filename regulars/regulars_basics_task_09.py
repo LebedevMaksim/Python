44e8bc6e60ea687a163aa5c3ca8fca5021648423
Makz82 <mackz@tut.by>
2023-01-05 16:22:01 +0300

    Regular expressions - Task 9 - Code

--- regulars/regulars_basics_task_09.py
+++ regulars/regulars_basics_task_09.py
@@ -16,8 +16,20 @@ text = [
 
 
 def main(text_):
-    pass
+    sub_line = re.sub(r'[^еЕёЁуУяЯыЫаАоОэЭиИюЮ/]', '', text_)
+    lines = re.split(r'/', sub_line)
+
+    if len(lines) != 3:
+        return 'Не хайку. Должно быть 3 строки.'
+
+    for line in range(len(lines)):
+        step = 5 + 2 * (line % 2)  # 5 7 5
+        if len(lines[line]) != step:
+            return f"Не хайку. В {line + 1} строке слогов не {step}, а {len(lines[line])}."
+
+    return 'Хайку!'
 
 
 if __name__ == '__main__':
-    main(text)
+    for i in text:
+        print(i, ' ' * (70 - len(i)), main(i))
